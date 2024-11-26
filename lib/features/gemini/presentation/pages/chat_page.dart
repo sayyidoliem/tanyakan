d3130bc8c55b7f6110fb5434fa9c7a28e5e3018c
Sayyid Olim <sayyid.olim12@gmail.com>
2024-11-26 14:12:15 +0700

    Gemini and chat succesfully show

--- lib/features/gemini/presentation/pages/chat_page.dart
+++ lib/features/gemini/presentation/pages/chat_page.dart
@@ -1,21 +1,229 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:tanyakan/features/gemini/presentation/controller/chat_text_gemini_controller.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:tanyakan/common/decoration.dart';
+import 'package:tanyakan/features/gemini/presentation/widgets/message_widget.dart';
+import 'package:tanyakan/helper/env.dart';
 
 @RoutePage()
-class ChatPage extends ConsumerWidget {
+class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    TextEditingController _textController = TextEditingController();
-    final fetchGemini =
-        ref.watch(fetchChatTextGeminiControllerProvider(_textController.text));
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  late final GenerativeModel _model;
+  late final ChatSession _chat;
+  final ScrollController _scrollController = ScrollController();
+  final TextEditingController _textController = TextEditingController();
+  final FocusNode _textFieldFocus = FocusNode(debugLabel: 'TextField');
+  bool _loading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _model = GenerativeModel(
+      model: 'gemini-1.5-flash',
+      apiKey: Env.geminiKey,
+      generationConfig: GenerationConfig(
+        temperature: 1,
+        topK: 40,
+        topP: 0.95,
+        maxOutputTokens: 8192,
+        responseMimeType: 'text/plain',
+      ),
+    );
+    _chat = _model.startChat();
+  }
+
+  void _scrollDown() {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) => _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(
+          milliseconds: 750,
+        ),
+        curve: Curves.easeOutCirc,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final history = _chat.history.toList();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Tanyakan'),
       ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: ListView.builder(
+                controller: _scrollController,
+                itemBuilder: (context, idx) {
+                  final content = history[idx];
+                  final text = content.parts
+                      .whereType<TextPart>()
+                      .map<String>((e) => e.text)
+                      .join('');
+                  return MessageWidget(
+                    text: text,
+                    isFromUser: content.role == 'user',
+                  );
+                },
+                itemCount: history.length,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 25,
+                horizontal: 15,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      autofocus: true,
+                      focusNode: _textFieldFocus,
+                      decoration:
+                          textFieldDecoration(context, 'Enter a prompt...'),
+                      controller: _textController,
+                      onSubmitted: (String value) {
+                        _sendChatMessage(value);
+                      },
+                    ),
+                  ),
+                  const SizedBox.square(dimension: 15),
+                  if (!_loading)
+                    IconButton(
+                      onPressed: () async {
+                        _sendChatMessage(_textController.text);
+                      },
+                      icon: Icon(
+                        Icons.send,
+                        color: Theme.of(context).colorScheme.primary,
+                      ),
+                    )
+                  else
+                    const CircularProgressIndicator(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _sendChatMessage(String message) async {
+    setState(() {
+      _loading = true;
+    });
+
+    try {
+      final response = await _chat.sendMessage(
+        Content.text(message),
+      );
+      final text = response.text;
+
+      if (text == null) {
+        _showError('Empty response.');
+        return;
+      } else {
+        setState(() {
+          _loading = false;
+          _scrollDown();
+        });
+      }
+    } catch (e) {
+      _showError(e.toString());
+      setState(() {
+        _loading = false;
+      });
+    } finally {
+      _textController.clear();
+      setState(() {
+        _loading = false;
+      });
+      _textFieldFocus.requestFocus();
+    }
+  }
+
+  void _showError(String message) {
+    showDialog<void>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Something went wrong'),
+          content: SingleChildScrollView(
+            child: Text(message),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            )
+          ],
+        );
+      },
     );
   }
 }
+
+
+//@RoutePage()
+// class ChatPage extends ConsumerWidget {
+//   const ChatPage({super.key});
+
+//   @override
+//   Widget build(BuildContext context, WidgetRef ref) {
+//     TextEditingController _textController = TextEditingController();
+//     final ScrollController _scrollController = ScrollController();
+
+//     void _scrollDown() {
+//       WidgetsBinding.instance.addPostFrameCallback(
+//         (_) => _scrollController.animateTo(
+//           _scrollController.position.maxScrollExtent,
+//           duration: const Duration(
+//             milliseconds: 750,
+//           ),
+//           curve: Curves.easeOutCirc,
+//         ),
+//       );
+//     }
+
+//     final fetchGemini =
+//         ref.watch(fetchChatTextGeminiControllerProvider(_textController.text));
+//         final history = fetchGemini.asData.value.
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Tanyakan'),
+//       ),
+//       body: Padding(
+//         padding: EdgeInsets.all(8.0),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             Expanded(
+//                 child: ListView.builder(
+//               controller: _scrollController,
+//               itemBuilder: (context, index) {
+//                 final context = 
+//               },
+//             ))
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
